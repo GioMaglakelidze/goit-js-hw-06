666295718a226afdded13bb6fce7f601f7b48d8c
GioMaglakelidze <giorgio1335@gmail.com>
2023-02-13 23:16:48 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,13 @@
 const sizeControl = document.querySelector("input#font-size-control");
 const mainText = document.querySelector("span#text");
 
-//Задаємо стандартний розмір (сподіваюсь на це :) )
+// Задаємо початковий розмір
 mainText.style.fontSize = sizeControl.value + "px";
-// Додавання слухача подій
-sizeControl.addEventListener("input", (e) => {
-  mainText.style.fontSize = `${e.target.value}px`; // зміна розміру тексту
-});
+
+// Вішаємо слухача на подію input
+sizeControl.addEventListener("input", fontSizeChange);
+// Створюємо зовнішню функцію для зміни розміру шрифта тексту в span
+function fontSizeChange() {
+  // задаємо інлайн стиль шрифта значенням положення повзунка поля
+  mainText.style.fontSize = sizeControl.value + "px";
+}
